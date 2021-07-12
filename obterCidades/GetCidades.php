de5a4788be5c50d00f24baf951ebc9d3ae787bdd
Marcio Teixeira <engmt@hotmail.com>
2021-07-12 17:21:56 -0300

    atualizando obter cidades

--- obterCidades/GetCidades.php
+++ obterCidades/GetCidades.php
@@ -5,23 +5,21 @@ $password = "";
 $database = "cidadesbase";
 
 $conn = new mysqli($servername, $username, $password, $database);
-
 if ($conn->connect_error) {
   die("Falha de conecxão: " . $conn->connect_error);
 }
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $estado = $_GET["estado"];
-	$sql ="SELECT idestado, nome FROM cidade WHERE idestado IN (SELECT id FROM estado WHERE uf='$estado')";
+	$sql ="SELECT nome, idestado FROM cidade WHERE idestado IN (SELECT id FROM estado WHERE uf='$estado')";
 	$resultado = $conn->query($sql);
 	if ($resultado->num_rows > 0) {
 
-	while ($linha = mysqli_fetch_assoc($resultado)) {
-            
-	  echo $linha["nome"];
-	}
-  }else{
-	  echo " Digite um estado válido";
+	while ($linha = $resultado->fetch_assoc()) {
+      $cidades[]= $linha['nome'];
+	} 
+      echo json_encode($cidades); 
   }
+    
 }
 
 ?>
